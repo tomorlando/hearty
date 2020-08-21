f8be0862743d7007a9ce5312edefbfc8bbaa1198
tom-orlando <tomorlando.02@gmail.com>
2020-08-21 11:21:21 +1000

    Changes to pre_processing and new data file

--- pre_processing/data_cleaning.r
+++ pre_processing/data_cleaning.r
@@ -1,10 +1,9 @@
 
-
 rm(list = ls())
 setwd("C:\\Users\\Tom Orlando\\Monash\\FIT3164")
 
-install.packages("readxl")
-install.packages("dplyr")
+# install.packages("readxl")
+# install.packages("dplyr")
 
 library(readxl)
 library("dplyr")
@@ -14,10 +13,8 @@ heart <- read_excel('./data/initial_dataset.xlsx')
 dim(heart)
 summary(heart)
 str(heart)
-View(heart)
 
 # Missing value processing -- if there is  null value,someone will be true
-is.na(heart)
 table(is.na(heart))
 sum(is.na(heart))
 
@@ -28,53 +25,48 @@ heart <- heart[,-which(colnames(heart) %in% c("Exertional CP"))]
 num <- select_if(heart, is.numeric)
 chara <- select_if(heart, is.character)
 summary(num)
-summary(chara)
+
+# Renaming columns with spaces
+df <- as_tibble(heart)
+
+df <- df %>% rename(
+    airway_disease = `Airway disease`,
+    thyroid_disease = `Thyroid Disease`,
+    weak_periph_pulse = `Weak Peripheral Pulse`,
+    lung_rales = `Lung rales`,
+    systolic_murmur = `Systolic Murmur`,
+    diastolic_murmur = `Diastolic Murmur`,
+    lowth_ang = `LowTH Ang`,
+    poor_r_progression = `Poor R Progression`,
+    current_smoker = `Current Smoker`,
+    typical_chest_pain = `Typical Chest Pain`,
+    function_class = `Function Class`,
+    q_wave = `Q Wave`,
+    st_elevation = `St Elevation`,
+    st_depression = `St Depression`,
+    region_rwma = `Region RWMA`
+  )
 
 # Encoding categorical data
-#encoding sex
-heart$Sex=factor(heart$Sex,levels = c('Fmale','Male'),labels = c(0,1))
-#encoding obesity
-heart$Obesity=factor(heart$Obesity,levels = c('Y','N'),labels = c(1,0))
-#encoding CRF
-heart$CRF=factor(heart$CRF,levels = c('Y','N'),labels = c(1,0))
-#encoding CVA
-heart$CVA=factor(heart$CVA,levels = c('Y','N'),labels = c(1,0))
-#encoding Airway disease
-heart$`Airway disease`=factor(heart$`Airway disease`,levels =c('Y','N'),labels = c(1,0))
-#encoding Thryoid Disease
-heart$`Thyroid Disease`=factor(heart$`Thyroid Disease`,levels =c('Y','N'),labels = c(1,0))
-#endoing CHF
-heart$CHF=factor(heart$CHF,levels =c('Y','N'),labels = c(1,0))
-#encoding DLP
-heart$DLP=factor(heart$DLP,levels =c('Y','N'),labels = c(1,0))
-#encoding Weak peripheral pulse
-heart$`Weak Peripheral Pulse`=factor(heart$`Weak Peripheral Pulse`,levels =c('Y','N'),labels = c(1,0))
-#encoding Lung rales
-heart$`Lung rales`=factor(heart$`Lung rales`,levels =c('Y','N'),labels = c(1,0))
-#encoding systolic murmur
-heart$`Systolic Murmur`=factor(heart$`Systolic Murmur`,levels =c('Y','N'),labels = c(1,0))
-#encoding  diastolic murmur
-heart$`Diastolic Murmur`=factor(heart$`Diastolic Murmur`,levels =c('Y','N'),labels = c(1,0))
-#encoding DYSPNEA
-heart$Dyspnea=factor(heart$Dyspnea,levels =c('Y','N'),labels = c(1,0))
-#encoding  ATYPICAL
-heart$Atypical=factor(heart$Atypical,levels =c('Y','N'),labels = c(1,0))
-#encoding nonanginal
-heart$Nonanginal=factor(heart$Nonanginal,levels =c('Y','N'),labels = c(1,0))
-#encoding lowth ang
-heart$`LowTH Ang`=factor(heart$`LowTH Ang`,levels =c('Y','N'),labels = c(1,0))
-#encoding LVH
-heart$LVH=factor(heart$LVH,levels =c('Y','N'),labels = c(1,0))
-#encoding poor r progression
-heart$`Poor R Progression`=factor(heart$`Poor R Progression`,levels =c('Y','N'),labels = c(1,0))
-#encoding BBB
-heart$BBB=factor(heart$BBB,levels =c('LBBB','N','RBBB'),labels = c(0,1,2))
-#encoding VHD
-heart$VHD=factor(heart$VHD,levels =c('N','mild','Moderate','Severe'),labels = c(0,1,2,3))
-#encoding Cath
-heart$Cath=factor(heart$Cath,levels =c('Cad','Normal'),labels = c(1,0))
+df.mm <- model.matrix(~Sex+Obesity+CRF+CVA+airway_disease+thyroid_disease+CHF+DLP+weak_periph_pulse+
+                          lung_rales+systolic_murmur+diastolic_murmur+Dyspnea+Atypical+Nonanginal+
+                          lowth_ang+LVH+poor_r_progression+BBB+VHD, data = df)
+df.mm <- df.mm[,-1] # removing "(intercept) column"
 
-write.csv(heart,"heart.csv",row.names = FALSE)
-nheart<- read.csv('./heart.csv')
+# Delete all previous non-encoded version of categorical columns
+df <- df[, -which(colnames(df) %in% c("Sex", "Obesity", "CRF", "CVA", "airway_disease","thyroid_disease","CHF","DLP","weak_periph_pulse",
+                          "lung_rales","systolic_murmur","diastolic_murmur","Dyspnea","Atypical","Nonanginal",
+                          "lowth_ang","LVH","poor_r_progression","BBB","VHD"))]
+
+# Combine dummy variables with initial columns 
+df.combined <- cbind(df, df.mm)
+df.combined$CAD_Yes=factor(df.combined$Cath,levels =c('Cad','Normal'),labels = c(1,0))
+df.combined <- df.combined[, -which(colnames(df.combined) %in% c("Cath"))]
+
+View(df.combined)
+
+# Save down file to a new csv
+write.csv(df.combined,"./data/heart_clean.csv", row.names = FALSE)
+nheart<- read.csv('./data/heart_clean.csv')
 
 str(nheart)
