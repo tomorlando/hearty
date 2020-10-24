bf3c511fed2412652d131b85eddd0bb1ea72d7c6
abrarhamzah <abrarfauzanhamzah@gmail.com>
2020-10-24 11:36:16 +0700

    Stepwise - Clean up the code and add documentation

--- models/feature_selection/stepwise/stepwise.r
+++ models/feature_selection/stepwise/stepwise.r
@@ -1,4 +1,17 @@
+############################################
+# File      : stepwise.r
+# Project   : FIT3164 project
+#
+# Date      : 03/09/2020
+# Author    : Abrar Fauzan Hamzah
+# 
+# Purpose   : Implementing stepwise to get 
+#             important features and analyse
+#             the AIC.
+#             Lower AIC the better.
+############################################
 
+#import training set
 x_train = read.csv('./data/heart_train.csv')
 
 ##BACKWARDS
@@ -7,22 +20,15 @@ fitAll <- lm(x_train$CAD_Yes ~., data = x_train)
 #summary(fitAll)
 
 step.back <- step(fitAll, direction = "backward")
-#formula(step.back)
 
-#'Age', 'Weight', 'Length', 'BMI', 'DM', 'HTN', 'FH', 'PR', 'typical_chest_pain', 'Tinversion', 'FBS', 'TG', 'BUN', 'ESR', 'K', 'Na', 'WBC', 'EF.TTE', 'region_rwma', 'SexMale', 'DLPY', 'lung_ralesY', 'diastolic_murmurY', 'lowth_angY', 'LVHY', 'BBBN', 'VHDModerate'
-
-    # AIC = -477.19
-    # 27 features selected
+# To get the formula uncomment the code below
+# formula(step.back)
 
 ##FORWARD
-
 fitStart <- lm(x_train$CAD_Yes ~ 1, data=x_train)
-#step.forward <- step(fitStart, direction="forward", scope=formula(fitAll))
+step.forward <- step(fitStart, direction="forward", scope=formula(fitAll))
 
-#'typical_chest_pain', 'region_rwma', 'HTN', 'Age', 'FBS', 'TG' ,'FH' , 'EF.TTE' , 'BBBN', 'DLPY', 'diastolic_murmurY', 'K', 'Tinversion', 'DM'
-
-    #AIC: -476.17
-    #14 Features
+# formula(step.forward)
 
 
 
